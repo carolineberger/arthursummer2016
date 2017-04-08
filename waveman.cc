59d1ffa95c0187af2f8a6221e94b4179e87def10
Caroline Berger <caroline.berger2@mail.mcgill.ca>
2017-04-08 11:58:31 -0400

    added triangle wave and man quit on q
    fix the mouth

--- waveman.cc
+++ waveman.cc
@@ -1,5 +1,30 @@
-
+#include <stdio.h>
+#include<stdlib.h>
 #include "glut.h"
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <math.h>
+int pID;
+void drawCircle(float r1, float r2, float pos1, float pos2){
+    glBegin(GL_TRIANGLE_FAN);
+	for(int ii = 0; ii < 100; ii++) { 
+		float theta = 2.0f * 3.1415926f * ii / 100;//get the current angle 
+		float x = r1 * cosf(theta);//calculate the x component 
+		float y = r2 * sinf(theta);//calculate the y component 
+		glVertex2f(x + pos1, y + pos2);//output vertex 
+    } 
+	glEnd(); 
+}
+
 void drawBack()
 {   
      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -17,15 +42,64 @@ void drawBack()
      glRectf(0.0f, -0.2f, 1.0f, -1.0f);
 
      //easier way of doing a triangle?
-     GLfloat vertices[] = {
-     0.0f, -0.2f, 0.0f,
-     0.0f, -0.3f, 0.0f,
-     -0.2f,  -0.2f, 0.0f
-};  
+     glBegin(GL_TRIANGLES);
+        glVertex3f(0, -.2, 0);
+        glVertex3f(1, -0.2, 0);
+        glVertex3f(0, 1, 0);
+    glEnd();
 
-     glEnd();
-     glutSwapBuffers();
+    glBegin(GL_TRIANGLES);
+        glVertex3f(0, .5, 0);
+        glVertex3f(-.5, .5, 0);
+        glVertex3f(0, 1, 0);
+    glEnd();
+    glBegin(GL_TRIANGLES);
+        glVertex3f( -.1,.5, 0);
+        glVertex3f(-.5, .5, 0);//stays the same
+        glVertex3f(-.5, -.2, 0);
+    glEnd();
+
+
+
+    //draw Man
+    glColor4f(1.0, 1.0, 0.0, 1.0); 
+    drawCircle (.045, .06, -.2, .13);
+    glLineWidth(2.5); 
+    glColor4f(1.0, 1.0, 0.0, 1.0); 
+    glBegin(GL_LINES);
+    //body
+    glVertex3f(-.2, .07, 0.0);
+    glVertex3f(-.2, -.1, 0.0);
+
+    glVertex3f(-.2, -.1, 0.0);
+    glVertex3f(-.17, -.2, 0.0);
+
+    glVertex3f(-.2, -.1, 0.0);
+    glVertex3f(-.23, -.2, 0.0);
+
+
+    glVertex3f(-.2, 0, 0.0);
+    glVertex3f(-.17, .06, 0.0);
+
+    glVertex3f(-.2, 0, 0.0);
+    glVertex3f(-.23, .06, 0.0);
+
+
+    //draw mouth
+    glColor3f(0.0, 0.0, 0.0); 
+    drawCircle (.03, .02, -.2, .13);
+
+    glEnd();
+    glutSwapBuffers();
     
+}
+void pressQ (unsigned char key, int xmouse, int ymouse){
+    switch (key){
+		case 'q':
+			glutDestroyWindow (pID);
+            exit (0);
+            break;
+    }
 }
 
 int main(int argc, char **argv)
@@ -34,7 +108,8 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_SINGLE);
     glutInitWindowSize(800, 500);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Wave Man");
+    pID = glutCreateWindow("Wave Man");
+    glutKeyboardFunc(pressQ);
     glutDisplayFunc(drawBack);
     glutMainLoop();
     return 0;
